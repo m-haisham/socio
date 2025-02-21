9806d0877a68bc13acc1b6a9889ffcac2cd30a4e
Haisham <47662901+m-haisham@users.noreply.github.com>
2025-02-21 18:39:37 +0500

    feat: implement UserAwareSocioProvider for OpenId with code exchange functionality

--- socio_providers/src/openid.rs
+++ socio_providers/src/openid.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use socio::{
     async_trait, error,
     jwt::verify_jwt_with_jwks_endpoint,
-    providers::{SocioProvider, StandardUser},
+    providers::{SocioProvider, StandardUser, UserAwareSocioProvider},
     types::{OpenIdTokenField, Response, SocioClient},
 };
 use url::Url;
@@ -34,6 +34,34 @@ impl SocioProvider for OpenId {
             .await?;
 
         let token = verify_jwt_with_jwks_endpoint::<StandardUser>(
+            &response.extra_fields().id_token,
+            &self.jwks_url.as_str(),
+            &client.client_id,
+        )
+        .await?;
+
+        Ok(Response::from_standard_token_response(
+            &response,
+            token.claims,
+        ))
+    }
+}
+
+#[async_trait]
+impl UserAwareSocioProvider for OpenId {
+    type User = serde_json::Value;
+
+    async fn exchange_code_for_user(
+        &self,
+        client: &SocioClient,
+        code: socio::oauth2::AuthorizationCode,
+        pkce_verifier: socio::oauth2::PkceCodeVerifier,
+    ) -> error::Result<Response<Self::User>> {
+        let response = client
+            .exchange_code::<OpenIdTokenField>(code, pkce_verifier)
+            .await?;
+
+        let token = verify_jwt_with_jwks_endpoint::<Self::User>(
             &response.extra_fields().id_token,
             &self.jwks_url.as_str(),
             &client.client_id,
